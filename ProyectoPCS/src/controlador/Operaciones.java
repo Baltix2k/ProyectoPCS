e889cd18623b43ea0274af22241525523e1c1a07
ricardorzan <62387274+ricardorzan@users.noreply.github.com>
2020-06-02 18:26:09 -0500

    Primeros cambios (Consistentes)

--- ProyectoPCS/src/controlador/Operaciones.java
+++ ProyectoPCS/src/controlador/Operaciones.java
@@ -1,7 +1,31 @@
 package controlador;
 
+import vista.Mensajes;
+
 public class Operaciones {
+    
+    Mensajes ms;
+    
+    public Operaciones() {
+        this.ms = new Mensajes();
+    }
+    
      public void ejecutarAplicacion() {
-        
+        int opcion = 0;
+        do {
+            ms.menu();
+            opcion = ms.opcionEnterio();
+            switch (opcion) {
+                case 1:
+                    ms.consultarMensaje();
+                    String matricula = ms.leerMatricula();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+            }
+        } while (opcion != 4);
     }
 }
