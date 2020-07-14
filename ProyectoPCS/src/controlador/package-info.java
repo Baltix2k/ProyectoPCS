6e981f1dee6ab9cb6fc27d9b1df5fa08ecce0547
Baltix2k <Baltix2k@users.noreply.github.com>
2020-07-13 22:00:12 -0500

    Documentacion de paquetes "package-info"

---
+++ ProyectoPCS/src/controlador/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Define las clases que serviran para comunicar la capa de modelo con la vista negociando entre
+ * los paquetes la informacion y realizar los procesos para aplicar los cambios a los datos, 
+ * funcionando como intermediario entre el modeo y la vista administrando el movimiento de 
+ * informacion entre las capas.
+ * 
+ */
+package controlador;
